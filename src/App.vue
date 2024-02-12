4b41e43597bc3833e1283c89f54e24944a2e5d3b
robot <cgajula152@gmail.com>
2024-02-12 15:31:29 +0530

    Made a seperate component for input

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" /><br />
-    <h4>Enter the comma seperated integers in the below tag</h4>
-    <form @submit.prevent="show = !show" action="">
-      <input required v-model="input" type="text" />
-      <input type="submit" />
-    </form>
-
-    <Color
-      v-if="show"
-      msg="Color Sorter"
-      :input="input"
-      :allowed_colors="allowed_colors"
-    />
+    <Color msg="Color Sorter" />
   </div>
 </template>
 
 <script>
 import Color from "./components/Color.vue";
-
 export default {
   name: "App",
-  data() {
-    return {
-      input: "",
-      show: false,
-      allowed_colors: [
-        "#32294D",
-        "#473E79",
-        "#5C4F89",
-        "#6b5e9a",
-        "#8171ad",
-        "#8f7fbc",
-        "#9d8cca",
-        "#ab9bd8",
-        "#bbaae7",
-        "#ccbdf4",
-        "#dacefa",
-        "#edd7ff",
-        "#f5eafe",
-      ],
-    };
-  },
   components: {
     Color,
   },
