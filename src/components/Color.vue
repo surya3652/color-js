9dba189fa4e8a1350b7f5498b1bf520eed99f8d0
robot <cgajula152@gmail.com>
2024-02-12 15:52:08 +0530

    Optimized the code

--- src/components/Color.vue
+++ src/components/Color.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
-    <table>
+    <h4>Enter the comma seperated integers in the below tag</h4>
+    <InputForm />
+    <table v-if="isVisible">
       <tr>
         <th>Number</th>
         <th>Corresponding Hex Code</th>
@@ -10,10 +11,24 @@
       </tr>
       <tr v-for="(element, index) in inputArray" :key="index">
         <td>{{ element }}</td>
-        <td>{{ colorMap[element] }}</td>
+        <td>
+          {{
+            mapping.find((e) => {
+              if (e.value === element) {
+                return true;
+              }
+            }).color
+          }}
+        </td>
         <td>
           <div
-            :style="{ backgroundColor: colorMap[element] }"
+            :style="{
+              backgroundColor: mapping.find((e) => {
+                if (e.value === element) {
+                  return true;
+                }
+              }).color,
+            }"
             class="small-div"
           ></div>
         </td>
@@ -23,50 +38,87 @@
 </template>
 
 <script>
+import InputForm from "./InputForm.vue";
 export default {
   name: "Color",
   props: {
     msg: String,
-    input: String,
-    allowed_colors: Array,
+  },
+  components: {
+    InputForm,
   },
   data() {
     return {
-      colorMap: {},
+      mapping: [],
       inputArray: [],
+      isVisible: false,
+      allowed_colors: [
+        "#32294D",
+        "#473E79",
+        "#5C4F89",
+        "#6b5e9a",
+        "#8171ad",
+        "#8f7fbc",
+        "#9d8cca",
+        "#ab9bd8",
+        "#bbaae7",
+        "#ccbdf4",
+        "#dacefa",
+        "#edd7ff",
+        "#f5eafe",
+      ],
     };
   },
-  created() {
-    var arr = this.input.split(",");
-    var colors = this.allowed_colors;
-    var sorted_arr = [...new Set(arr)];
-    var mapping = {};
 
-    if (sorted_arr.length == colors.length) {
-      arr.forEach((ele) => {
-        if (!mapping[ele]) {
-          mapping[ele] = colors.shift();
+  methods: {
+    compute(input) {
+      this.inputArray = input;
+      var colors = this.allowed_colors;
+      var sorted_arr = [...new Set(this.inputArray)];
+      var arr_len = sorted_arr.length;
+      var col_len = colors.length;
+
+      if (arr_len == col_len) {
+        this.inputArray.forEach((ele) => {
+          if (this.mapping.value != ele) {
+            this.mapping.push({
+              value: ele,
+              color: colors.shift(),
+            });
+          }
+        });
+      } else if (arr_len < col_len) {
+        var x = Math.floor(col_len / arr_len);
+        var counter = 0;
+        /* counter variable is intialized to stop assigning colors to input
+      values when input values comes to an end
+      Ex : col_len = 13 , arr_len = 5
+      so the step size should be 2 i.e. 0,2,4,6,8 indices of color array are
+      taken to assign them to 5 values.
+      The loop usually runs upto i reaches 12, but it should stop when i = 8.
+      so maintained a counter variable to stop iteration thereafter.
+      */
+        for (let i = 0; i < col_len && counter != arr_len; i += x) {
+          if (this.mapping.value != sorted_arr[counter]) {
+            this.mapping.push({
+              value: sorted_arr[counter],
+              color: colors[i],
+            });
+          }
+          counter++;
         }
-      });
-    } else if (sorted_arr.length <= colors.length) {
-      var x = Math.floor(colors.length / sorted_arr.length);
-      var counter = 0;
-      for (
-        let i = 0;
-        i < colors.length && counter != sorted_arr.length;
-        i += x
-      ) {
-        mapping[sorted_arr[counter++]] = colors[i];
+      } else {
+        for (let i = 0; i < arr_len; i++) {
+          if (this.mapping.value != sorted_arr[i]) {
+            this.mapping.push({
+              value: sorted_arr[i],
+              color: colors[i % col_len],
+            });
+          }
+        }
       }
-    } else {
-      for (let i = 0; i < sorted_arr.length; i++) {
-        mapping[sorted_arr[i]] = colors[(i % (colors.length))];
-
-      }
-    }
-    console.log("Hi")
-    this.colorMap = mapping;
-    this.inputArray = this.input.split(",");
+      // console.log(JSON.parse(JSON.stringify(this.mapping)));
+    },
   },
 };
 </script>
@@ -81,8 +133,8 @@ td {
   margin-left: auto;
   margin-right: auto;
 }
-td{
-  font-weight:800;
+td {
+  font-weight: 800;
 }
 .small-div {
   height: 20px;
---
+++ src/components/InputForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="" action="">
+      <input required v-model="input" type="text" />
+      <button @click="trigger()">Show</button>
+      <button @click="reset()">Reset</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputForm",
+  data() {
+    return {
+      input: "",
+      isVisible: false,
+    };
+  },
+  methods: {
+    trigger() {
+      if (this.input != "") {
+        this.$parent.isVisible = true;
+      }
+      this.$parent.compute(this.input.split(","));
+    },
+    reset() {
+      this.input = "";
+      this.$parent.isVisible = false;
+    },
+  },
+};
+</script>
